4c63d3c5f07eaeec3483f7ac939a19fe24551851
Polina <polinmailbox@gmail.com>
2019-01-27 15:48:56 +0300

    Initial Commit

---
+++ iTunes Previews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunes Previews
+//
+//  Created by Polina Dulko on 1/27/19.
+//  Copyright © 2019 Polina Dulko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
